d4489b4f7ca71453a9ef81f07e8f036c99583331
Stanislav Shakirov <punksta@protonmail.com>
2018-03-06 23:49:22 +0300

    remove react-native-fast-image; upgrade build tools

--- android/app/src/main/java/com/thecatnative/MainApplication.java
+++ android/app/src/main/java/com/thecatnative/MainApplication.java
@@ -3,7 +3,6 @@ package com.thecatnative;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.dylanvann.fastimage.FastImageViewPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -23,8 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new FastImageViewPackage()
+          new MainReactPackage()
       );
     }
 
