66c4e5c40047bac48affdce4dcb0c1d70b2d5151
Stanislav Shakirov <punksta@protonmail.com>
2018-03-30 13:36:02 +0300

    add new logo, splash screen to android

--- android/app/src/main/java/com/thecatnative/MainActivity.java
+++ android/app/src/main/java/com/thecatnative/MainActivity.java
@@ -4,6 +4,18 @@ import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setTheme(R.style.AppTheme);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        setTheme(R.style.AppTheme_Splash);
+    }
+
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
